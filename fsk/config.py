1e6c4cdd48562dc4b042e52e7d562170b1d47f1c
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-09-06 16:53:07 +0200

    add vilt to distance computation

--- fsk/config.py
+++ fsk/config.py
@@ -2,5 +2,6 @@
 layers = {
     'albef_img': [f'hs_{n}' for n in range(12)] + ['c_out'],
     'albef_multi': [f'hs_{n}' for n in range(6)],
-    'clip_img': [f'hs_{n}' for n in range(12)] + ['c_out']
+    'clip_img': [f'hs_{n}' for n in range(12)] + ['c_out'],
+    'vilt_multi': [f'hs_{n}' for n in range(12)]
 }
