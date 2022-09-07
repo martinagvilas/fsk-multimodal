6d4e0f4f3ba5ac497184ef4b2ed591a19d9cdda8
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-09-07 16:42:49 +0200

    add slurm file

---
+++ slurm/itm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=itm
+#SBATCH --begin=now
+#SBATCH --partition=GPUshort
+#SBATCH --output=/mnt/hpc/home/gonzalezvm/slurm/%A-%a.out
+#SBATCH --error=/mnt/hpc/home/gonzalezvm/slurm/%A-%a.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=martinagonzalezvilas@gmail.com
+
+module purge
+module load conda/4.10.1
+conda activate fsk
+cd /mnt/hpc/home/gonzalezvm/multimodal-fsk/fsk/it_match/
+srun python run.py -m vilt -pp /mnt/hpc/home/gonzalezvm/fsk-multimodal/
