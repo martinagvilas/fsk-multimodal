6bdb59039a1bf51e47c7c0c122f895ddfe3e308f
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-09-10 08:03:41 +0200

    minor change to mcrae extraction

--- fsk/dataprep/mcrae.py
+++ fsk/dataprep/mcrae.py
@@ -20,24 +20,3 @@ def load_mcrae_synsets_info(annot_path):
         regex=r"\.", value='-'
     )
     return synsets_info
-
-
-# def get_sem_feature_matrix(annot_path):
-#     mcrae_file = annot_path / 'mcrae_features_info.txt'
-#     mcrae_info = pd.read_csv(mcrae_file, sep="\t")
-#     mcrae_info['Feature'] = (
-#         mcrae_info['Feature'].replace(to_replace=r".+_-_", value='', regex=True)
-#     )
-#     mcrae_info['Concept'] = mcrae_info['Concept'].replace(to_replace={
-#         'bin_(waste_)': 'wastebin', 'board_(black)': 'blackboard',
-#         'hose_(leggings)': 'leggings', 'drapes': 'curtains', 'nylons': 'nylon', 
-#         'onions': 'onion', 'pajamas': 'pajama'
-#     })
-#     f_matrix = pd.DataFrame(
-#         np.zeros((len(concepts), len(features))), 
-#         index=concepts, columns=features
-#     )
-#     for _, row in mcrae_info.iterrows():
-#         if row['Concept'] in concepts:
-#             f_matrix.loc[row['Concept'], row['Feature']] = row['Prod_Freq']
-#     return f_matrix
