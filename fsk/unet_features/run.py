b6c9f6a2c9236a83f4fe56f4d6e1f9e21deac939
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-09-10 08:04:03 +0200

    add vit extraction

---
+++ fsk/unet_features/run.py
@@ -0,0 +1,22 @@
+import argparse
+from pathlib import Path
+
+import torch
+
+from fsk.unet_features.vit import FtVit
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    m_help = 'Select which model to run. Can be one of the following options: \
+        vit_16, vit_32, bert, gpt'
+    parser.add_argument('-m', action='store', required=True, help=m_help)
+    p_help = 'Path to the project folder containing the dataset and source code'
+    parser.add_argument('-pp', action='store', required=True, help=p_help)
+    model = parser.parse_args().m
+    project_path = Path(parser.parse_args().pp)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    if model.startswith('vit'):
+        version = model.split('_')[1]
+        FtVit(version, project_path, device=device).compute()
---
+++ fsk/unet_features/vit.py
@@ -0,0 +1,47 @@
+from tqdm import tqdm
+import torch
+from torch.utils.data import DataLoader
+from transformers import ViTFeatureExtractor, ViTModel
+
+from fsk.dataprep.dataloader import ThingsDataset
+
+
+class FtVit():
+    def __init__(self, version, project_path, batch_idx=None, device='cpu'):
+        self.version = version
+        self.model_name = f'vit_{version}'
+        self.layers = {'img': [f'encoder.layer.{n}' for n in range(12)]}
+        self.device = device
+
+        self.dataset_path = project_path / 'dataset'
+        self.res_path = project_path / 'results' / self.model_name / 'net_ft'
+        self.res_path.mkdir(parents=True, exist_ok=True)
+    
+        self.load_model()
+        self.get_dataset(batch_idx)
+    
+    def load_model(self):
+        if self.version == '32':
+            source = 'google/vit-base-patch32-224-in21k'
+        elif self.version == '16':
+            source = 'google/vit-base-patch16-224-in21k'
+        self.img_transform = ViTFeatureExtractor.from_pretrained(source)
+        self.model = ViTModel.from_pretrained(
+            source, output_hidden_states=True
+        ).to(self.device).eval()
+
+    def get_dataset(self, batch_idx):
+        ds = ThingsDataset(self.dataset_path, batch_idx=batch_idx)
+        self.dataset = DataLoader(
+            ds, batch_size=None, batch_sampler=None, collate_fn=lambda x: x
+        )
+
+    def compute(self):
+        for _, data in tqdm(enumerate(self.dataset), total=len(self.dataset)):
+            img_ft = self.img_transform(data['img'], return_tensors="pt")
+            with torch.no_grad():
+                out = self.model(**img_ft)
+                hs = torch.squeeze(torch.stack(out.hidden_states)[1:])
+                hs_file = self.res_path / f"hs_img_{data['img_id']}.pt"
+                torch.save(hs, hs_file)
+
