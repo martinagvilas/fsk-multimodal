6d9886f3f588ab10f508ca9c4bae00e74cfaab34
Martina Vilas <martinagonzalezvilas@gmail.com>
2022-09-13 23:10:10 +0200

    add visualization of results

--- fsk/unet_features/gpt.py
+++ fsk/unet_features/gpt.py
@@ -2,12 +2,13 @@ from nltk.corpus import wordnet as wn
 import torch
 from transformers import GPT2Tokenizer, GPT2Model
 
-from fsk.dataprep.utils import get_concepts, get_fsk_synsets
+from fsk.dataprep.utils import get_concepts, get_fsk_synsets, get_synsets_ids
 
 
 class FtGPT():
-    def __init__(self, project_path, device='cpu'):
+    def __init__(self, project_path, caption_type, device='cpu'):
         self.model_name = 'gpt'
+        self.caption_type = caption_type
         self.layers = {'txt': [f'h.{n}' for n in range(12)]}        
         self.device = device
 
@@ -33,22 +34,24 @@ class FtGPT():
         self.dataset = {}
         for c, s in zip(concepts, synsets):
             s_wn = wn.synset(('.').join(s.split('-')))
-            s_def = f"{c} is a {s_wn.definition()}"
-            self.dataset[s] = [c, s_def]
+            if self.caption_type == 'definition':
+                data = f"{c} is a {s_wn.definition()}"
+            else:
+                data = c
+            self.dataset[s] = data
     
     def compute(self):
-        for c_idx, caption_type in enumerate(['concept', 'definition']):
-            hs = []
-            for s_vals in self.dataset.values():
-                txt = s_vals[c_idx] + "<|endoftext|>"
-                tokens = self.processor(
-                    txt, truncation=True, return_tensors="pt"
-                )
-                with torch.no_grad():
-                    out = self.model(**tokens)
-                # Use EOS token as CLIP
-                hs.append(
-                    torch.squeeze(torch.stack(out.hidden_states)[1:, :, -1, :])
-                )
-            hs_file = self.res_path / f'hs_txt_{caption_type}.pt'
-            torch.save(torch.stack(hs), hs_file)
+        hs = []
+        for s_vals in self.dataset.values():
+            txt = s_vals[c_idx] + "<|endoftext|>"
+            tokens = self.processor(
+                txt, truncation=True, return_tensors="pt"
+            )
+            with torch.no_grad():
+                out = self.model(**tokens)
+            # Use EOS token as CLIP
+            hs.append(
+                torch.squeeze(torch.stack(out.hidden_states)[1:, :, -1, :])
+            )
+        hs_file = self.res_path / f'hs_txt_{self.caption_type}.pt'
+        torch.save(torch.stack(hs), hs_file)
