b2270a01f3083ef333fe02fd6b06e343507831d8
t9s9 <schaumloeffel.tim@web.de>
2022-09-23 14:27:57 +0200

    add ViT classifier and ImageNet concept mapping

--- fsk/unet_features/run.py
+++ fsk/unet_features/run.py
@@ -4,24 +4,31 @@ from pathlib import Path
 import torch
 
 from fsk.unet_features.language import FtBert, FtGPT
-from fsk.unet_features.vit import FtVit
+from fsk.unet_features.vit import FtVit, ClsVit
 
-
-if __name__ == '__main__':    
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     m_help = 'Select which model to run. Can be one of the following options: \
-        vit_16, vit_32, bert, gpt'
+        vit_16, vit_32, bert, gpt, cls_vit_16, cls_vit_32'
     parser.add_argument('-m', action='store', required=True, help=m_help)
     p_help = 'Path to the project folder containing the dataset and source code'
     parser.add_argument('-pp', action='store', required=True, help=p_help)
-    model = parser.parse_args().m
-    project_path = Path(parser.parse_args().pp)
+    bs_help = 'Specify batch size for classification models'
+    parser.add_argument('-bs', action='store', required=False, help=bs_help, type=int, default=32)
+
+    args = parser.parse_args()
+    model = args.m
+    project_path = Path(args.pp)
+    batch_size = args.bs
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     if model.startswith('vit'):
         version = model.split('_')[1]
         FtVit(version, project_path, device=device).compute()
+    elif model.startswith('cls'):
+        version = model.split('_')[2]
+        ClsVit(version, project_path, batch_size=batch_size, device=device).compute()
     elif model == 'bert':
         FtBert(project_path, text_type='concept').compute()
     elif model == 'gpt':
--- fsk/unet_features/vit.py
+++ fsk/unet_features/vit.py
@@ -1,25 +1,27 @@
-from tqdm import tqdm
 import torch
+from timm import create_model
+from timm.data import create_transform, IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 from transformers import ViTFeatureExtractor, ViTModel
 
 from fsk.dataprep.dataloader import ThingsDataset
 
 
 class FtVit():
-    def __init__(self, version, project_path, batch_idx=None, device='cpu'):
+    def __init__(self, version, project_path, result_dir: str = 'net_ft', batch_idx=None, device='cpu'):
         self.version = version
         self.model_name = f'vit_{version}'
         self.layers = {'img': [f'encoder.layer.{n}' for n in range(12)]}
         self.device = device
 
         self.dataset_path = project_path / 'dataset'
-        self.res_path = project_path / 'results' / self.model_name / 'net_ft'
+        self.res_path = project_path / 'results' / self.model_name / result_dir
         self.res_path.mkdir(parents=True, exist_ok=True)
-    
+
         self.load_model()
         self.get_dataset(batch_idx)
-    
+
     def load_model(self):
         if self.version == '32':
             source = 'google/vit-base-patch32-224-in21k'
@@ -47,3 +49,40 @@ class FtVit():
                 hs_file = self.res_path / f"hs_img_{data['img_id']}.pt"
                 torch.save(hs, hs_file)
 
+
+class ClsVit(FtVit):
+    def __init__(self, version, project_path, result_dir='net_cls', batch_size=32, batch_idx=None, device='cpu'):
+        self.batch_size = batch_size
+        super().__init__(version, project_path, result_dir, batch_idx, device)
+
+    def get_dataset(self, batch_idx):
+        ds = ThingsDataset(self.dataset_path, batch_idx=batch_idx)
+        self.dataset = DataLoader(ds, batch_size=self.batch_size,
+                                  collate_fn=lambda x: (
+                                      torch.stack([self.img_transform(i['img']) for i in x]),
+                                      [i['img_id'] for i in x])
+                                  )
+
+    def load_model(self):
+        if self.version == '32':
+            model_name = 'vit_base_patch32_224'
+        elif self.version == '16':
+            model_name = 'vit_base_patch16_224'
+        self.img_transform = create_transform(input_size=224,
+                                              interpolation='bicubic',
+                                              crop_pct=0.9,
+                                              mean=IMAGENET_INCEPTION_MEAN,
+                                              std=IMAGENET_INCEPTION_STD,
+                                              is_training=False)
+        self.model = create_model(model_name, pretrained=True).to(self.device).eval()
+
+    def compute(self):
+        logits, img_idx = [], []
+        for img, img_id in tqdm(self.dataset, total=len(self.dataset)):
+            with torch.no_grad():
+                out = self.model(img.to(self.device))
+                logits.append(out.cpu())
+                img_idx.extend(img_id)
+        logits = torch.concat(logits)
+        torch.save(logits, self.res_path / 'logits.pt')
+        torch.save(img_idx, self.res_path / 'image_ids.pt')
